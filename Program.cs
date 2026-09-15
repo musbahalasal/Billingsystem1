26fb1b344d405ce3d26881d085635ec42c52887a
musbahalasal <146354544+musbahalasal@users.noreply.github.com>
2026-09-15 23:55:12 +0300

    Refactor customer and invoiceitem classes with constructors

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Billingsystem1.invoiceitem;
 
 namespace Billingsystem1
 {
@@ -10,18 +12,10 @@ namespace Billingsystem1
     {
         static void Main(string[] args)
         {
-            customer c1 = new customer();
-            c1.id = 123;
-            c1.name = "musbah";
-            c1.age = 21;
-            invoiceitem item1 = new invoiceitem();
-            item1.productname = "keyboard";
-            item1.price = 150;
-            item1.quantity = 1; 
-            invoice inv =new invoice();
-            inv.invoiceid = 1;
-            inv.item= item1;
-            inv.customer = c1;
+            customer c1 = new customer(123, "musbah", 21);
+            invoiceitem item1 = new invoiceitem("keyboard", 150, 1);
+            invoice inv = new invoice(c1, 1, item1);
+
             Console.WriteLine("info about the invoice ");
             Console.WriteLine("Customer Name: " + inv.customer.name);
             Console.WriteLine("Product. " + inv.item.productname);
@@ -30,22 +24,68 @@ namespace Billingsystem1
             Console.ReadLine();
         }
     }
+
     public class customer
     {
-        public int id;
-        public string name;
-        public int age;
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public customer(int id, string name, int age)
+        {
+            if (age < 0)
+            {
+                Console.WriteLine("note the age cant be in negative");
+            }
+            else
+            {
+                this.age = age;
+            }
+            this.id = id;
+            this.name = name;
+
+        }
     }
     public class invoiceitem
     {
-        public string productname;
-        public decimal price;
-        public int quantity;
-    }
-    public class invoice
-    {
-        public customer customer;
-        public int invoiceid;
-        public invoiceitem item;
+        public string productname { get; set; }
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+        public invoiceitem(string productname, decimal price, int quantity)
+        {
+            this.productname = productname;
+            if (price < 0)
+            {
+
+                Console.WriteLine("note the price given in negative");
+                this.price = 0;
+            }
+            else { this.price = price; }
+            if (quantity < 0)
+            {
+                Console.WriteLine("note:wrong quantity");
+                this.quantity = 1;
+            }
+            else
+            {
+                this.quantity = quantity;
+            }
+        }
+
+        public decimal Getsubtotal()
+        {
+            return price * quantity;
+        }
+        public class invoice
+        {
+            public int invoiceid { get; set; }
+            public customer customer { get; set; }
+            public invoiceitem item { get; set; }
+            public invoice(int invoiceid, customer customer, invoiceitem item)
+            {
+                this.invoiceid = invoiceid;
+                this.customer = customer;
+                this.item = item;
+            }
+        }
     }
 }
